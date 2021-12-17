6ca19330d228e1746201f5236a86734c2d7b13bf
D3NCyan <edwarddoltons@gmail.com>
2021-12-17 17:48:54 +0800

    create routing for homepage

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { 
+  BrowserRouter as Router,  
+  Route,
+  Switch
+} from 'react-router-dom'
+
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      Halo
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
